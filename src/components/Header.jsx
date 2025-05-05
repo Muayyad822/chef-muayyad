cd07b67d8b3cf46c7300a451372cde5f1db8554f
AbdulMuiz Jimoh <muayyad822@gmail.com>
2025-05-05 20:22:10 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <>
+            <header className="header">
+                <img src="/chef-icon.png" alt="Chef Icon" />
+                <h1>Chef Muayyad</h1>
+            </header>
+        </>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+export default function Main(){
+   const [ingredients, setIngredients] = useState([])
+
+   const ingredientsListItems = ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+   ) )
+
+   function addIngredient(formData){
+        const newIngredients = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredients])
+
+   }
+
+
+    return (
+        <>
+            <main>
+                <form action={addIngredient}>
+                    <input 
+                    aria-label="Add ingredient" 
+                    type="text" 
+                    placeholder="e.g beef" 
+                    name="ingredient"
+                 
+                    />
+                    <button > Add ingredient</button>
+                </form>
+                { ingredients.length > 0 && 
+                <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                { ingredients.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div> }
+            </section> }
+               
+                
+            </main>
+        </>
+    )
+}
+
